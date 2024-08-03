9cfaae1d3efaff944f5f30eb53f54dcd5518ce96
Ahmad Khalifa AbdUlraouf <132180439+khalifa2003@users.noreply.github.com>
2024-08-03 15:15:12 +0300

    navbar and landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { MenubarModule } from 'primeng/menubar';
+import { LandingComponent } from './landing/landing.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, LandingComponent],
+  imports: [BrowserModule, AppRoutingModule, MenubarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
