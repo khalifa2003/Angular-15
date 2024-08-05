3ab610477fd656b52f40a0715ec32334343e58f8
Ahmad Khalifa AbdUlraouf <132180439+khalifa2003@users.noreply.github.com>
2024-08-05 12:25:58 +0300

    Angular gallery component in landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+// Primng
 import { MenubarModule } from 'primeng/menubar';
 import { LandingComponent } from './landing/landing.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { GalleriaModule } from 'primeng/galleria';
+
 @NgModule({
   declarations: [AppComponent, NavbarComponent, LandingComponent],
-  imports: [BrowserModule, AppRoutingModule, MenubarModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MenubarModule,
+    InputTextModule,
+    ButtonModule,
+    GalleriaModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -3,8 +3,56 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent {
+  images: any[];
 
+  responsiveOptions: any[] = [
+    {
+      breakpoint: '1024px',
+      numVisible: 5,
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 3,
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1,
+    },
+  ];
+
+  constructor() {
+    this.images = [
+      {
+        itemImageSrc: '../../assets/swipper/swipper1.jpg',
+        thumbnailImageSrc: '../../assets/swipper/swipper1.jpg',
+      },
+      {
+        itemImageSrc: '../../assets/swipper/swipper2.jpg',
+        thumbnailImageSrc: '../../assets/swipper/swipper2.jpg',
+      },
+      {
+        itemImageSrc: '../../assets/swipper/swipper3.jpg',
+        thumbnailImageSrc: '../../assets/swipper/swipper3.jpg',
+      },
+      {
+        itemImageSrc: '../../assets/swipper/swipper4.jpg',
+        thumbnailImageSrc: '../../assets/swipper/swipper4.jpg',
+      },
+      {
+        itemImageSrc: '../../assets/swipper/swipper5.jpg',
+        thumbnailImageSrc: '../../assets/swipper/swipper5.jpg',
+      },
+      {
+        itemImageSrc: '../../assets/swipper/swipper6.jpg',
+        thumbnailImageSrc: '../../assets/swipper/swipper6.jpg',
+      },
+      {
+        itemImageSrc: '../../assets/swipper/swipper7.webp',
+        thumbnailImageSrc: '../../assets/swipper/swipper7.webp',
+      },
+    ];
+  }
 }
