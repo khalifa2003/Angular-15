5e0c2ec4fc409eee439321f8f59f37430dbf8cb1
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2024-08-26 06:48:31 +0300

    update Height of pages

--- src/app/components/User/check-out/check-out.component.ts
+++ src/app/components/User/check-out/check-out.component.ts
@@ -1,4 +1,8 @@
+import { AddressService } from 'src/app/services/address.service';
+import { AuthService } from 'src/app/services/auth.service';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IUser } from 'src/app/Models/iuser';
 
 @Component({
   selector: 'app-check-out',
@@ -6,6 +10,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./check-out.component.css'],
 })
 export class CheckOutComponent {
+  userForm: FormGroup = new FormGroup({});
+  user: IUser = {} as IUser;
+  showModal: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private addressService: AddressService
+  ) {}
+  ngOnInit(): void {
+    this.user = this.authService.currentUserValue.data;
+    this.userForm = this.fb.group({
+      fname: [this.user.fname, Validators.required],
+      lname: [this.user.lname, Validators.required],
+      email: [this.user.email, [Validators.required, Validators.email]],
+      phone: [this.user.phone, Validators.required],
+      address: [, Validators.required],
+      city: [, Validators.required],
+      country: [, Validators.required],
+      state: ['Alexandria', Validators.required],
+      postalCode: [],
+    });
+  }
+  get f() {
+    return this.userForm.controls;
+  }
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  onSubmit() {
+    if (this.userForm.invalid) {
+      return;
+    }
+
+    this.addressService.addAddress(this.userForm.value).subscribe((res) => {
+      this.openModal();
+      this.userForm.reset();
+    });
+  }
+  // -------------------------------------------------
   addresses = ['01015388310 - Alexandria - شارع العوضي'];
   selectedAddress = this.addresses[0];
   newAddress = { mobile: '', city: '', address: '' };
