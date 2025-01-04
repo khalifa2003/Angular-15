bb9b185c06145e90c414a54b948491a65abf4486
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2025-01-04 03:38:59 +0200

    update API Url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://programming-area.up.railway.app',
-  apiUrl: 'http://localhost:3000/',
+  apiUrl: 'https://programming-area.zeabur.app',
 };
