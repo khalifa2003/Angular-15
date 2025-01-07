9dcb40e4bf0a0f88fb6f3c159dcf4d82baa63ee1
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2025-01-08 01:11:29 +0200

    updateURL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://programming-area-api.vercel.app',
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://programming-area-api.vercel.app',
+  // apiUrl: 'http://localhost:3000',
 };
